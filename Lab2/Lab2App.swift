8de1eae281f93fb045bb7d8d6458dbb847b8d490
Andrew Yee <ios@hashbrownlabs.com>
2025-10-21 23:28:06 -0700

    Initial Commit

---
+++ Lab2/Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2App.swift
+//  Lab2
+//
+//  Created by Andrew Yee on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
